c9678b09e9d1de4cd87a5a4409b857d7c70dbcd9
josegonzcs <jose.gonz.cs@gmail.com>
2025-10-15 18:50:03 -0700

    Start round of 32 matchup logic.

---
+++ bracket.js
@@ -0,0 +1,24 @@
+// read matchups csv
+async function readMatchups(url) {
+  const response = await fetch(url);
+  const data = await response.text();
+  return parseCSV(data);
+}
+
+function parseCSV(data) {
+  var rawdata = data.split("\r\n");
+  var matchups = new Map();
+  rawdata.forEach((line) => {
+    var qualifying_teams = line.split(",");
+    var matchup_teams = qualifying_teams.splice(8, 8);
+    matchups.set(qualifying_teams.join(""), matchup_teams);
+  });
+  console.log(matchups.get("ABCDEFGH"));
+  return matchups;
+}
+
+const rankings = await readMatchups("/matchups.csv");
+// make a list of passing team combinations and map them to the correct matchup
+// update bracket
+
+// 1Aopponent,1Bopponent,1Dopponent,1Eopponent,1Gopponent,1Iopponent,1Kopponent,1Lopponent
